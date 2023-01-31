60b11458e25adac3d31d9524293107b1fe65de64
RealView45 <kjink015@gmail.com>
2023-02-01 04:49:34 +0900

    Initial Commit

---
+++ SwiftUI Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI Test
+//
+//  Created by 김진경 on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
